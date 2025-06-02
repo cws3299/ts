1935c1ccec4d9a3b1bf5c88b6c15fab9af1b1aa0
ws <cws3299@gmail.com>
2025-06-02 15:49:01 +0900

    ts활용cra webpack설정, babel설정

---
+++ 2week/spotifyDemo/spotify-demo/webpack.config.js
@@ -0,0 +1,92 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+
+  return {
+    mode: isProduction ? "production" : "development",
+    entry: path.resolve(__dirname, "src", "index.tsx"),
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"], // CSS 파일 처리
+        },
+        {
+          test: /\.svg$/,
+          use: [
+            {
+              loader: "@svgr/webpack",
+              options: {
+                icon: true,
+              },
+            },
+            {
+              loader: "file-loader",
+              options: {
+                name: "[name].[ext]?ver=[hash]",
+                outputPath: "images",
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(png|jpg|gif|jpeg)$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                name: "[name].[ext]?ver=[hash]",
+                outputPath: "images",
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(ts|js)x?$/, // .tsx 또는 .jsx 파일을 babel-loader를 통해 변환함
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: "babel-loader",
+            },
+          ],
+        },
+      ],
+    },
+    output: {
+      // 번들링된 파일이 저장될 위치와 이름을 지정함
+      path: path.resolve(__dirname, "dist"),
+      chunkFilename: "[name].js?ver=[hash]",
+      filename: "[name].js?ver=[hash]",
+      publicPath: "/",
+    },
+    devtool: isProduction ? "source-map" : "inline-source-map",
+    devServer: {
+      port: 3000,
+      open: true,
+      hot: true,
+      historyApiFallback: true,
+    },
+    optimization: {
+      minimize: isProduction,
+    },
+    plugins: [
+      new CleanWebpackPlugin(), // 이전 빌드 파일을 삭제
+      new HtmlWebpackPlugin({
+        // HTML 파일을 생성하고, 번들링된 자바스크립트를 자동으로 포함 시킴
+        template: path.resolve(__dirname, "src", "index.html"), // 해당 파일 필수
+      }),
+      new Dotenv({
+        // 환경 변수 파일로드 관련 내용임
+        allowEmptyValues: true,
+        systemvars: true,
+      }),
+    ],
+  };
+};
