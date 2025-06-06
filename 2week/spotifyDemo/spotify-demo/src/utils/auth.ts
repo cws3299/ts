002749f1166de907b548baa3396ca006fc7ebbd3
ws <cws3299@gmail.com>
2025-06-06 14:08:34 +0900

    redirectUri가 잘못된 곳이 있어 수정

--- 2week/spotifyDemo/spotify-demo/src/utils/auth.ts
+++ 2week/spotifyDemo/spotify-demo/src/utils/auth.ts
@@ -1,4 +1,5 @@
 import { CLIENT_ID } from "../config/authConfig";
+import { RedirectUri } from "../config/commonConfig";
 import { AuthUrlParams } from "../models/auth";
 import { base64encode, generateRandomString, sha256 } from "./crypto";
 
@@ -8,7 +9,7 @@ export const getSpotifyAuthUrl = async () => {
   const codeChallenge = base64encode(hashed);
 
   const clientId = CLIENT_ID;
-  const redirectUri = "http://127.0.0.1:3000";
+  const redirectUri = RedirectUri;
 
   const scope = "user-read-private user-read-email"; // api permission
   const authUrl = new URL("https://accounts.spotify.com/authorize");
